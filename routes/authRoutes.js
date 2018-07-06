1c775bc91d8fbecc99bf1491e94aea9c9d6740fd
ortizk <ortizk@live.com>
2018-07-06 15:58:30 -0700

    added logout

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,6 +9,13 @@ module.exports = (app) => {
 
 	app.get('/auth/google/callback', passport.authenticate('google'));
 
+	app.get('api/logout', (req, res) => {
+		// logout is a function provided by passport. 
+		// takes the cookie and kills the id in it
+		req.logout();
+		res.send(req.user);
+	})
+
 	app.get('/api/current_user', (req, res) => {
 		res.send(req.user);
 	});
